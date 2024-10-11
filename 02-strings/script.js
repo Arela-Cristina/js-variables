d2bc8ff4e7a33c503f4e12165943326d4c7ed366
Arela-Cristina <instagrandine@gmail.com>
2024-10-11 15:47:55 +0200

    first exercise strings done

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,6 +1,8 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
+let name = 'Cri';
 // Stampa il valore di nome nella console.
+console.log('This is my name:', name);
 
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
